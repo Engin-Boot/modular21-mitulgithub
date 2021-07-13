1cc503149a1386fa5e40f808d0d562ff2e1ccad9
mitulgithub <35561984+mitulgithub@users.noreply.github.com>
2021-07-13 15:26:31 +0530

    Add files via upload

---
+++ TelCo.ColorCoder/ColorMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    class ColorMap
+    {
+        public static Color[] colorMapMajor;
+        public static Color[] colorMapMinor;
+       
+        static ColorMap()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+    }
+}
